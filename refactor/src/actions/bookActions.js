338ba377a26aff16c3985c09e690d5b38f5fffd4
zktnguyen <zktnguyen@ucdavis.edu>
2017-11-05 03:00:41 -0800

    adding redux files to project

---
+++ refactor/src/actions/bookActions.js
@@ -0,0 +1,8 @@
+
+
+// export constants for books reducer
+export const POST_BOOK = 'POST_BOOK';
+export const DELETE_BOOK = 'DELETE_BOOK';
+export const GET_BOOKS = 'GET_BOOKS';
+export const UPDATE_BOOK = 'UPDATE_BOOK';
+
